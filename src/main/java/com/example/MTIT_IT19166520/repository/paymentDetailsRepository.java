6a6c67ab9a83b4f088c6762de1f45e89c3e4fbb2
SurekhaMaduhansi <68699681+SurekhaMaduhansi@users.noreply.github.com>
2022-06-14 22:56:07 +0530

    initial commit

---
+++ src/main/java/com/example/MTIT_IT19166520/repository/paymentDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.example.MTIT_IT19166520.repository;
+
+import com.example.MTIT_IT19166520.model.paymentDetails;
+import org.springframework.data.repository.CrudRepository;
+
+public interface paymentDetailsRepository extends CrudRepository<paymentDetails, Integer> {
+}
