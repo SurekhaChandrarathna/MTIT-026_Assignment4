2f99f0634123a4fe5f5853e2490d05587b93b78c
SurekhaMaduhansi <68699681+SurekhaMaduhansi@users.noreply.github.com>
2022-06-14 23:00:59 +0530

    initial commit

---
+++ src/main/java/com/example/MTIT_IT19177892/repository/userRepository.java
@@ -0,0 +1,7 @@
+package com.example.MTIT_IT19177892.repository;
+
+import com.example.MTIT_IT19177892.model.user;
+import org.springframework.data.repository.CrudRepository;
+
+public interface userRepository extends CrudRepository<user, Integer> {
+}
---
+++ src/test/java/com/example/MTIT_IT19177892/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MTIT_IT19177892;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
